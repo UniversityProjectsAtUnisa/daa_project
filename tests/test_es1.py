f93881c0e210a8a904b57cd4dafda3ac04fa6e31
Marco Della Rocca <marco741147@gmail.com>
2021-12-03 02:28:21 +0100

    Added tests

--- tests/test_es1.py
+++ tests/test_es1.py
@@ -1,14 +1,40 @@
 import pytest
+import random
 from .. import es1
 
 
-def test_one():
-    arr = [1, 7, 2, 8, 4, 6, 9, 4]
-    res = es1.lds(arr)
-    assert res == [8, 6, 4]
+def bruteforce(arr):
+    best = []
+    best_value = 0
+    for i in range(2**len(arr)):
+        cur = []
+        for j, taken in enumerate(f"{i:>0{len(arr)+2}b}"[2:]):
+            if taken == "1":
+                if len(cur) and arr[j] > cur[-1]:
+                    break
+                cur.append(arr[j])
+        if sum(cur) > best_value:
+            best = cur
+            best_value = sum(cur)
+
+    return best
 
 
-def test_two():
-    arr = [4, 4, 3, 7, 5]
-    res = es1.lds(arr)
-    assert res == [7, 5]
+def tests_multiple_random():
+    for _ in range(20):
+        r = [0] * 20
+        for i in range(len(r)):
+            r[i] = random.randint(0, 50)
+
+        expected = bruteforce(r)
+        assert expected == es1.lds(r)
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ([1, 7, 2, 8, 4, 6, 9, 4], [8, 6, 4]),
+    ([4, 4, 3, 7, 5], [7, 5]),
+    ([1, 7, 2, 8, 4, 6, 9, 4, 9], [9, 9]),
+])
+def test_success(test_input, expected):
+    result = es1.lds(test_input)
+    assert sum(result) == sum(expected), f"{result} {expected}"
